78b90075c285d0cbe0c7834ccb29c386324e693e
Aaron Samuel <aasamuelj@gmail.com>
2017-09-29 02:46:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@artists = []
+  @@artists, @@genres = [], []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << genre
     @@count +=1
     @@artists << artist
   end
